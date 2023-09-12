440abbe58e5a58145d9c26c04cc8d04ca58fdf2d
mehwishjbe <mehwish.jbe@gmail.com>
2023-09-12 15:03:28 +0200

    getElementById

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 // Document Objects
 // console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 'DOM'
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[9]);
+// document.all[9].textContent = 'hello';
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
 
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-document.title = 'DOM'
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.all[9]);
-document.all[9].textContent = 'hello';
 
 
-console.log(document.forms);
-console.log(document.links);
+
+/* getElementById */
+// var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
+// headerTitle.textContent = 'hello';
+// headerTitle.innerText = 'bye bye';
+// headerTitle.innerHTML = '<h3>Ciao</h3>';

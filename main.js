8580db8fdfff7ad87c178e770c7d2b83e81d30ac
mehwishjbe <mehwish.jbe@gmail.com>
2023-09-12 15:22:14 +0200

    getElementsByTagName

--- main.js
+++ main.js
@@ -34,15 +34,30 @@
 
 
 /* getElementsByClassName */
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].innerText = 'Item Ciao'
-items[1].style.color = 'red'
-items[1].style.fontWeight = 'bold'
-items[1].style.backgroundColor = 'yellow'
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].innerText = 'Item Ciao'
+// items[1].style.color = 'red'
+// items[1].style.fontWeight = 'bold'
+// items[1].style.backgroundColor = 'yellow'
 
-for(let i = 0; i<items.length; i++){
-    items[i].style.backgroundColor = 'gray'
+// for(let i = 0; i<items.length; i++){
+//     items[i].style.backgroundColor = 'gray'
+// }
+
+
+
+
+/* getElementsByTagName */
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].innerText = 'Item Ciao'
+li[1].style.color = 'red'
+li[1].style.fontWeight = 'bold'
+li[1].style.backgroundColor = 'yellow'
+
+for(let i = 0; i<li.length; i++){
+    li[i].style.backgroundColor = 'gray';
 }
-

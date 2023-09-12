5f0e758f4cf5867b37b8302f2b1510ac1dd151bd
mehwishjbe <mehwish.jbe@gmail.com>
2023-09-12 15:18:56 +0200

    getElementsByClassName

--- main.js
+++ main.js
@@ -27,5 +27,22 @@
 // var headerTitle = document.getElementById('header-title');
 // headerTitle.style.borderBottom = 'solid 3px white'
 
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px darkgreen'
+// var header = document.getElementById('main-header');
+// header.style.borderBottom = 'solid 3px darkgreen'
+
+
+
+
+/* getElementsByClassName */
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].innerText = 'Item Ciao'
+items[1].style.color = 'red'
+items[1].style.fontWeight = 'bold'
+items[1].style.backgroundColor = 'yellow'
+
+for(let i = 0; i<items.length; i++){
+    items[i].style.backgroundColor = 'gray'
+}
+
